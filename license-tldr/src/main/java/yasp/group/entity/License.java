973d244c2f7b95775fb9ff6f2061e2b0c6c5c358
Hanicef <gustaf.alhall@yahoo.com>
2018-05-03 14:46:11 +0200

    Added license JPA entity

---
+++ license-tldr/src/main/java/yasp/group/entity/License.java
@@ -0,0 +1,37 @@
+
+package yasp.group.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="license")
+public class License implements Serializable {
+   @Id
+   @GeneratedValue(strategy=GenerationType.IDENTITY)
+   @Column(name="id", nullable=false)
+   private int id;
+
+   @Column(name="name", length=64)
+   private String name;
+
+   @Column(name="sourceURL", length=256)
+   private String sourceURL;
+
+   public License() {
+   }
+
+   public License(int id, String name, String sourceURL) {
+      this.id = id;
+      this.name = name;
+      this.sourceURL = sourceURL;
+   }
+
+   public int getID() { return this.id; }
+   public String getName() { return this.name; }
+   public String getSourceURL() { return this.sourceURL; }
+
+   public void setID(int id) { this.id = id; }
+   public void setName(String name) { this.name = name; }
+   public void setSourceURL(String sourceURL) { this.sourceURL = sourceURL; }
+}
