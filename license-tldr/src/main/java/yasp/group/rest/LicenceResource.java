748c713b25bab57c74a2606fc2c26415e046223a
Hanicef <gustaf.alhall@yahoo.com>
2018-05-18 08:38:49 +0200

    Replaced spaces with tabs

--- license-tldr/src/main/java/yasp/group/rest/LicenceResource.java
+++ license-tldr/src/main/java/yasp/group/rest/LicenceResource.java
@@ -27,7 +27,7 @@ public class LicenceResource {
 	}
 	@GET
 	@Path("/license/{id}")
-    @Produces("application/json")
+	@Produces("application/json")
 	public Response getLicenseById(@PathParam("id") int id) {
 		License result = service.getLicenseById(id);
 		return Response.ok(result).build();
@@ -52,14 +52,14 @@ public class LicenceResource {
 	}
 	@GET
 	@Path("/summary/{id}")
-    @Produces("application/json")
+	@Produces("application/json")
 	public Response getSummaryById(@PathParam("id") int id) {
 		Summary result = service.getSummaryById(id);
 		return Response.ok(result).build();
 	}
 	@GET
 	@Path("/sumfromlic/{id}")
-    @Produces("application/json")
+	@Produces("application/json")
 	public Response getSummaryFromLicense(@PathParam("id") int id) {
 		License l = service.getLicenseById(id);
 		List<Summary> result = service.getSummariesFromLicense(l.getId());
@@ -67,7 +67,7 @@ public class LicenceResource {
 	}
 	@GET
 	@Path("/sumfromlicid/{id}")
-    @Produces("application/json")
+	@Produces("application/json")
 	public Response getSummaryByLicenseId(@PathParam("id") int id) {
 		List<Summary> result = service.getSummariesFromLicense(id);
 		return Response.ok(result).build();
@@ -77,28 +77,28 @@ public class LicenceResource {
 	//POST
 	@POST
 	@Path("/license/{createLicense}")
-    @Consumes("application/json")
+	@Consumes("application/json")
 	public Response createLicense(@PathParam("createLicense")JSONObject json) {
-	    try {
-            License license = new License(json.getString("name"), json.getString("sourceURL"));
-            service.createLicense(license);
-            return Response.ok().build();
-        }catch (Exception e){
-            return Response.status(400).build();
-        }
+		try {
+			License license = new License(json.getString("name"), json.getString("sourceURL"));
+			service.createLicense(license);
+			return Response.ok().build();
+		}catch (Exception e){
+			return Response.status(400).build();
+		}
 	}
 
 	@POST
 	@Path("/summary/{summaryjson}")
-    @Consumes("application/json")
+	@Consumes("application/json")
 	public Response createSummary(@PathParam("summaryjson")JSONObject json) {
-	    try {
-            Summary summary = new Summary(json.getString("name"),json.getString("description"));
-            service.createSummary(summary);
-            return Response.ok().build();
-        }catch (Exception e){
-	        return Response.status(400).build();
-        }
+		try {
+			Summary summary = new Summary(json.getString("name"),json.getString("description"));
+			service.createSummary(summary);
+			return Response.ok().build();
+		}catch (Exception e){
+			return Response.status(400).build();
+		}
 	}
 	@POST
 	@Path("/connectlicsum/{lid}/{sid}")
@@ -118,47 +118,47 @@ public class LicenceResource {
 
 
 	//PUT
-    @PUT
-    @Path("/editlicense/{license}")
-    @Consumes("application/json")
-    public Response editLicense(@PathParam("license") JSONObject json){
-	    License l = service.getLicenseById(json.getInt("id"));
-	    l.setName(json.getString("name"));
-	    l.setSourceURL(json.getString("sourceURL"));
-	    service.applyLicenseChanges(l);
-	    return Response.ok().build();
-    }
-    @PUT
-    @Path("/editsummary/{summary}")
-    @Consumes("application/json")
-    public Response editSummary(@PathParam("summary") JSONObject json){
-        Summary s = service.getSummaryById(json.getInt("id"));
-        s.setName(json.getString("name"));
-        s.setDescription(json.getString("description"));
-        service.applySummaryChanges(s);
-        return Response.ok().build();
-    }
+	@PUT
+	@Path("/editlicense/{license}")
+	@Consumes("application/json")
+	public Response editLicense(@PathParam("license") JSONObject json){
+		License l = service.getLicenseById(json.getInt("id"));
+		l.setName(json.getString("name"));
+		l.setSourceURL(json.getString("sourceURL"));
+		service.applyLicenseChanges(l);
+		return Response.ok().build();
+	}
+	@PUT
+	@Path("/editsummary/{summary}")
+	@Consumes("application/json")
+	public Response editSummary(@PathParam("summary") JSONObject json){
+		Summary s = service.getSummaryById(json.getInt("id"));
+		s.setName(json.getString("name"));
+		s.setDescription(json.getString("description"));
+		service.applySummaryChanges(s);
+		return Response.ok().build();
+	}
 
 
-    //DELETE
-    @DELETE
-    @Path("/del/li/{id}")
-    public Response deleteLicense(@PathParam("id") int id){
-	    service.deleteLicense(id);
-	    return Response.ok().build();
-    }
-    @DELETE
-    @Path("/del/su/{id}")
-    public Response deleteSummary(@PathParam("id") int id){
-        service.deleteSummary(id);
-        return Response.ok().build();
-    }
+	//DELETE
+	@DELETE
+	@Path("/del/li/{id}")
+	public Response deleteLicense(@PathParam("id") int id){
+		service.deleteLicense(id);
+		return Response.ok().build();
+	}
+	@DELETE
+	@Path("/del/su/{id}")
+	public Response deleteSummary(@PathParam("id") int id){
+		service.deleteSummary(id);
+		return Response.ok().build();
+	}
 
 
 	//TestMethod
 	@POST
 	@Path("/test")
-    @Produces("application/json")
+	@Produces("application/json")
 	public Response createTest() {
 		Summary summary = new Summary("TestSummary", "Ipsum Testum");
 		License license = new License("TestLicence","http://www.Thisgoesnowhere.now");
